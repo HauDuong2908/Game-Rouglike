614d07ad1b6756959e53b9cf4818991c7443f978
HauDuong2908 <dphau.20it2@vku.udn.vn>
2024-12-06 10:42:55 +0700

    player: Behaviour

--- Assets/Scripts/Behaviours/Water Drip/WaterDrip_IdleBehaviour.cs
+++ Assets/Scripts/Behaviours/Water Drip/WaterDrip_IdleBehaviour.cs
@@ -8,8 +8,6 @@ public class WaterDrip_IdleBehaviour : StateMachineBehaviour
     private float minReadyDrip, maxReadyDrip;
     private float lastReadyDripTime, readyDripTime;
     private WaterDrip drip;
-
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (drip == null)
@@ -18,8 +16,6 @@ public class WaterDrip_IdleBehaviour : StateMachineBehaviour
         readyDripTime = Random.Range(minReadyDrip, maxReadyDrip);
         lastReadyDripTime = Time.time + readyDripTime;
     }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (Time.time >= +lastReadyDripTime)
@@ -27,22 +23,4 @@ public class WaterDrip_IdleBehaviour : StateMachineBehaviour
             animator.SetTrigger("Drop");
         }
     }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
